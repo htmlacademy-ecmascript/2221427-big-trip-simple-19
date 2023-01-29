d97de9e3460b3e4de39e37e364750093d3668771
лев елистратов <msijkeee@gmail.com>
2023-01-29 22:10:59 +0700

    final

--- src/main.js
+++ src/main.js
@@ -1,17 +1,58 @@
-import { render } from './render.js';
-import TripPresenter from './presenter/trip-presenter.js';
-import TripFilterView from './view/trip-filter-view.js';
-import TripSortView from './view/trip-sort-view.js';
+import { render } from './framework/render.js';
+import NewPointButtonView from './view/new-point-button-view.js';
+import BoardPresenter from './presenter/board-presenter.js';
+import FilterPresenter from './presenter/filter-presenter.js';
+import PointsModel from './model/points-model.js';
+import PointCommonModel from './model/point-common-model.js';
+import FilterModel from './model/filter-model.js';
+import PointsApiService from './api/points-api-service.js';
+import PointCommonApiService from './api/point-common-api-service.js';
 
-const tripFilterContainer = document.querySelector('.trip-controls__filters');
-const siteTripContainer = document.querySelector('.trip-events');
+const AUTHORIZATION = 'Basic tsVaL7Ypbm9c4UVjKcef';
+const END_POINT = 'https://19.ecmascript.pages.academy/big-trip-simple';
 
-const tripPresenter = new TripPresenter({
-  siteContainer: siteTripContainer
+const mainContentElement = document.querySelector('.trip-events');
+const filterContainerElement = document.querySelector('.trip-controls__filters');
+const siteHeaderElement = document.querySelector('.trip-main');
+const pointsModel = new PointsModel({
+  pointsApiService: new PointsApiService(END_POINT, AUTHORIZATION)
+});
+const pointCommonModel = new PointCommonModel({
+  pointCommonApiService: new PointCommonApiService(END_POINT, AUTHORIZATION)
+});
+const filterModel = new FilterModel();
+
+const boardPresenter = new BoardPresenter({
+  boardContainer: mainContentElement,
+  pointsModel,
+  pointCommonModel,
+  filterModel,
+  onNewPointDestroy: handleNewPointFormClose,
+});
+const filterPresenter = new FilterPresenter({
+  filterContainer: filterContainerElement,
+  filterModel,
+  pointsModel
+});
+const newPointButtonComponent = new NewPointButtonView({
+  onClick: handleNewPointButtonClick
 });
 
-render(new TripFilterView(), tripFilterContainer);
-render(new TripSortView(), siteTripContainer);
+function handleNewPointFormClose() {
+  newPointButtonComponent.element.disabled = false;
+}
 
-tripPresenter.init();
+function handleNewPointButtonClick() {
+  boardPresenter.createPoint();
+  newPointButtonComponent.element.disabled = true;
+}
 
+filterPresenter.init();
+boardPresenter.init();
+
+Promise.all([
+  pointsModel.init(),
+  pointCommonModel.init()])
+  .then(() => {
+    render(newPointButtonComponent, siteHeaderElement);
+  });
